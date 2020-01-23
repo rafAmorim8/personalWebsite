eacb76b1295e052b794d89da17fba505167db4fd
rafAmorim8 <rafael_amorim30@hotmail.com>
2020-01-22 23:42:40 -0800

    Finishes contact section

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// Redirect to Portfolio section
+const goToPortfolio = () => {
+  window.location.replace("/index.html#portfolio");
+};
+
+// Redirect to Contact section
+const goToContact = () => {
+  window.location.replace("/index.html#contact");
+};
