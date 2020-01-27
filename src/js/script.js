53d485cced830b4499547ed33393d41c87e7a0c0
rafAmorim8 <rafael_amorim30@hotmail.com>
2020-01-27 10:59:49 -0800

    Adds scroll behaviour to navbar amd icons to portfolio card

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,27 @@ const goToPortfolio = () => {
 const goToContact = () => {
   window.location.replace("/index.html#contact");
 };
+
+// Show and hide navbar on scroll + add bg-color on 0 offset
+var lastScrollTop = 0;
+var navbar = document.getElementById("navbar");
+var menu = document.getElementById("menu");
+
+window.addEventListener("scroll", () => {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.classList.add("hide-nav");
+  } else {
+    navbar.classList.remove("hide-nav");
+  }
+
+  lastScrollTop = scrollTop;
+
+  if (lastScrollTop > 70) {
+    navbar.classList.add("nav-light");
+    menu.classList.add("menu-light");
+  } else {
+    navbar.classList.remove("nav-light");
+    menu.classList.remove("menu-light");
+  }
+});
